c2410d7f40e8d4dd4559aec3e21dd0fec44f93aa
amans8270 <amans8270@gmail.com>
2026-01-24 20:49:42 +0530

    Versel error check

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,21 +1,28 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define MONGODB_URI in environment variables");
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
-
-let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export async function connectDB() {
   if (cached.conn) return cached.conn;
 
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is not defined at runtime");
+  }
+
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then(mongoose => mongoose);
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
-  
 }
